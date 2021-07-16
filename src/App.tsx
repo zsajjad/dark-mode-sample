daa4addf4e2e57cdc4d5e1b031d67f265f955005
Zain Sajjad <zsajjad93@gmail.com>
2021-07-16 16:37:33 +0500

    fix: add image url

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import ColorSchemeToggle from './ColorScheme/ColorSchemeToggle';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+const IMAGE_URL = "https://images.unsplash.com/photo-1444567257257-a10a4088a89d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={"https://images.unsplash.com/photo-1444567257257-a10a4088a89d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"} className="App-logo" alt="logo" />
+        <a href={IMAGE_URL} 
+          target="_blank"
+          rel="noopener noreferrer">
+          <img src={IMAGE_URL} className="App-logo" alt="logo" />
+        </a>
         <ColorSchemeToggle />
         <a
           className="App-link"
